d60686942923bafee032812b62760ee23160c837
Shikigami Maito <shikigamimaito25@gmail.com>
2023-06-19 17:55:18 +0700

    Refactor modules using ES6 import/export

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,13 +1,11 @@
 // Drag & Drop Interfaces
-namespace App {
-  export interface Draggable {
-    dragStartHandler(event: DragEvent): void;
-    dragEndHanlder(event: DragEvent): void;
-  }
-  
-  export interface DragTarget {
-    dragOverHandler(event: DragEvent): void;
-    dropHandler(event: DragEvent): void;
-    dragLeaveHandler(event: DragEvent): void;
-  }
+export interface Draggable {
+  dragStartHandler(event: DragEvent): void;
+  dragEndHanlder(event: DragEvent): void;
+}
+
+export interface DragTarget {
+  dragOverHandler(event: DragEvent): void;
+  dropHandler(event: DragEvent): void;
+  dragLeaveHandler(event: DragEvent): void;
 }
