1d633018297cf4cbff43b1e6161ee44b645b4a65
Shikigami Maito <shikigamimaito25@gmail.com>
2023-06-19 17:10:53 +0700

    Refactor file structure

---
+++ src/components/base-component.ts
@@ -0,0 +1,41 @@
+namespace App {
+  // Component Base Class
+  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+    templateElement: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostELementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.templateElement = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostELementId)! as T;
+
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+      this.attach(insertAtStart);
+    }
+
+    private attach(insertAtBegining: boolean) {
+      if (insertAtBegining) {
+        this.hostElement.insertAdjacentElement("afterbegin", this.element);
+      } else {
+        this.hostElement.insertAdjacentElement("beforeend", this.element);
+      }
+    }
+
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ src/models/drag-drop.ts
@@ -0,0 +1,13 @@
+// Drag & Drop Interfaces
+namespace App {
+  export interface Draggable {
+    dragStartHandler(event: DragEvent): void;
+    dragEndHanlder(event: DragEvent): void;
+  }
+  
+  export interface DragTarget {
+    dragOverHandler(event: DragEvent): void;
+    dropHandler(event: DragEvent): void;
+    dragLeaveHandler(event: DragEvent): void;
+  }
+}
